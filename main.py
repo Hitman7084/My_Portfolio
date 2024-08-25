825f1c2022f6fbf225ab63249b63efbbe6873bab
Himanshu Mall <hitman7084@gmail.com>
2024-08-25 09:07:11 +0530

    Changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/about')
@@ -15,4 +15,4 @@ def projects():
     return render_template('projects.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(debug=True)
