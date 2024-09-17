f1dfec69109eb02eb80f850a0f8aa52e6bc119c8
Himanshu Mall <hitman7084@gmail.com>
2024-09-17 17:26:08 +0530

    reveal animation

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,3 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const container = document.querySelector('.container');
     container.style.opacity = 1;
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behaviour: 'smooth'
+        })
+    })
+})
